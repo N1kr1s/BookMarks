b3e6979ec9bf155afea0db48d8231b8bf5dd30bb
Nikris <nikris@mail.ru>
2022-07-03 18:20:51 +0300

    second phase

--- script.ts
+++ script.ts
@@ -1,38 +1,61 @@
-'use strict';
 class BookmarkMaker {
+  public modalIsOpen: boolean = false;
+  public modal: HTMLDivElement;
+  public showModal: HTMLHeadingElement;
+  public closeModal: HTMLElement;
+  public bookmarkForm: HTMLFormElement;
+  public bookmarksContainer: HTMLDivElement;
+  public deletePost!: HTMLElement;
+  public localArray: {
+    websiteName: string;
+    websiteURL: string;
+  }[] = [];
+
   constructor() {
-    this.modalIsOpen = false;
-    this.localArray = [];
-    this.modal = document.getElementById('modal');
-    this.showModal = document.getElementById('show-modal');
-    this.closeModal = document.getElementById('close-modal');
-    this.bookmarkForm = document.getElementById('bookmark-form');
-    this.bookmarksContainer = document.getElementById('bookmarks-container');
+    this.modal = document.getElementById('modal') as HTMLDivElement;
+    this.showModal = document.getElementById(
+      'show-modal'
+    ) as HTMLHeadingElement;
+    this.closeModal = document.getElementById('close-modal') as HTMLElement;
+    this.bookmarkForm = document.getElementById(
+      'bookmark-form'
+    ) as HTMLFormElement;
+    this.bookmarksContainer = document.getElementById(
+      'bookmarks-container'
+    ) as HTMLDivElement;
+
     this.showModal.addEventListener('click', () => {
       this.modalOpen();
     });
+
     this.closeModal.addEventListener('click', () => {
       this.modalClose();
     });
-    this.modal.addEventListener('click', (e) => {
+
+    this.modal.addEventListener('click', (e: Event) => {
       if (e.target === this.modal) this.modalClose();
     });
+
     this.bookmarkForm.addEventListener('submit', this.submitForm.bind(this));
   }
-  modalOpen() {
+
+  modalOpen(): void {
     this.modalIsOpen = true;
     this.modal.classList.add('show-modal');
   }
-  modalClose() {
+
+  modalClose(): void {
     this.modalIsOpen = false;
     this.modal.classList.remove('show-modal');
   }
-  submitForm(e) {
+
+  submitForm(e: Event) {
     this.bookmarkForm;
-    const target = e.target;
-    const websiteName = target[0].value;
-    const websiteURL = target[1].value;
+    const target = e.target as HTMLFormElement;
+    const websiteName = (target[0] as HTMLInputElement).value;
+    const websiteURL = (target[1] as HTMLInputElement).value;
     // *Saving to local storage
+
     this.localArray.push({ websiteName, websiteURL });
     localStorage.setItem('collection', JSON.stringify(this.localArray));
     const nodeToAppend = document.createElement('div');
@@ -52,4 +75,5 @@ class BookmarkMaker {
     this.bookmarksContainer.appendChild(nodeToAppend);
   }
 }
+
 const bookmarkMaker = new BookmarkMaker();
